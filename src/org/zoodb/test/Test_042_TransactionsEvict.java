7d2aa25aeab339c9ae29777c0c10ea87b55fe3b0
tilmann <tilmann@b278144e-e56a-4748-98cb-974f122dab01>
2011-11-02 21:28:57 +0000

    git-svn-id: https://svn.origo.ethz.ch/zoodb/trunk@305 b278144e-e56a-4748-98cb-974f122dab01

--- src/org/zoodb/test/Test_042_TransactionsEvict.java
+++ src/org/zoodb/test/Test_042_TransactionsEvict.java
@@ -3,6 +3,8 @@ package org.zoodb.test;
 import static junit.framework.Assert.assertEquals;
 import static org.junit.Assert.assertTrue;
 
+import java.lang.reflect.Field;
+
 import javax.jdo.JDOHelper;
 import javax.jdo.ObjectState;
 import javax.jdo.PersistenceManager;
@@ -108,6 +110,46 @@ public class Test_042_TransactionsEvict {
 
 		pm.currentTransaction().commit();
 		pm.close();
+	}
+	
+	@Test
+	public void testEvictAttributes() {
+		PersistenceManager pm = TestTools.openPM();
+		pm.currentTransaction().begin();
+		
+		TestClass tc = new TestClass();
+		tc.setInt(55);
+		tc.setRef1(tc);  //ref to self
+		pm.makePersistent(tc);
+		
+		pm.currentTransaction().commit();
+		pm.currentTransaction().begin();
+		
+		assertEquals(tc, tc.getRef1());
+		assertEquals(tc, getFieldValue("_ref1", tc));
+		pm.evict(tc);
+		assertEquals(null, getFieldValue("_ref1", tc));
+		assertEquals(tc, tc.getRef1());
+
+		pm.currentTransaction().commit();
+		pm.close();
+	}
+	
+	private Object getFieldValue(String fName, Object obj) {
+		try {
+			Class<?> cls = obj.getClass();
+			Field f = cls.getDeclaredField(fName);
+			f.setAccessible(true);
+			return f.get(obj);
+		} catch (NoSuchFieldException e) {
+			throw new RuntimeException(e);
+		} catch (SecurityException e) {
+			throw new RuntimeException(e);
+		} catch (IllegalArgumentException e) {
+			throw new RuntimeException(e);
+		} catch (IllegalAccessException e) {
+			throw new RuntimeException(e);
+		}
 	}
 	
 	@After
