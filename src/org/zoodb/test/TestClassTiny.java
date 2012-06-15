e97439a46b78b63455d3605662cb8121c0f3f454
ztilmann <ztilmann@CHATTAN.d.ethz.ch>
2012-06-15 17:36:38 +0200

    Updated copyright notice

--- src/org/zoodb/test/TestClassTiny.java
+++ src/org/zoodb/test/TestClassTiny.java
@@ -1,6 +1,24 @@
+/*
+ * Copyright 2009-2011 Tilmann Zäschke. All rights reserved.
+ * 
+ * This file is part of ZooDB.
+ * 
+ * ZooDB is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ * 
+ * ZooDB is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with ZooDB.  If not, see <http://www.gnu.org/licenses/>.
+ * 
+ * See the README and COPYING files for further information. 
+ */
 package org.zoodb.test;
-
-import junit.framework.Assert;
 
 import org.zoodb.jdo.spi.PersistenceCapableImpl;
 
@@ -9,26 +27,23 @@ public class TestClassTiny extends PersistenceCapableImpl {
 	private int _int;
 	private long _long;
 	
-	public void setData(int i, boolean flag, char c, byte b, short s, long l, String str, byte[] ba) {
-		_int = i;
-	}
-	
 	public void setInt(int i) {
+        zooActivateWrite();
 		_int = i;
 	}
 	
 	public void setLong(long l) {
+        zooActivateWrite();
 		_long = l;
 	}
 	
-	public void checkData(int i, boolean flag, char c, byte b, short s, long l, String str, byte[] ba) {
-		Assert.assertEquals(i, _int);
-	}
-	
 	public long getLong() {
+        zooActivateRead();
 		return _long;
 	}
+
 	public int getInt() {
+        zooActivateRead();
 		return _int;
 	}
 }
