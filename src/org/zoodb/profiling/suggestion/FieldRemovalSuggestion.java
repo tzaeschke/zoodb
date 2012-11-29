8d4ef1bd306ef6a3e529a85e334fb26d064b5e4e
tgmuender <tgmuender@gmail.com>
2012-11-29 09:16:33 +0100

    suggestion display

--- src/org/zoodb/profiling/suggestion/FieldRemovalSuggestion.java
+++ src/org/zoodb/profiling/suggestion/FieldRemovalSuggestion.java
@@ -2,14 +2,12 @@ package org.zoodb.profiling.suggestion;
 
 public class FieldRemovalSuggestion extends FieldSuggestion {
 	
-	private final String identifier = "FIELD_REMOVAL_SUGGESTION";
+	private final String identifier = "FIELD_REMOVAL";
 
 
 	public String getText() {
 		StringBuilder sb = new StringBuilder();
 		
-		sb.append(identifier);
-		sb.append(": ");
 		sb.append("Class=");
 		sb.append(getClazzName());
 		sb.append(",Field=");
@@ -20,6 +18,20 @@ public class FieldRemovalSuggestion extends FieldSuggestion {
 		sb.append(", never accessed");
 		
 		return sb.toString();
+	}
+	
+	public String provideLabelForColumn(int columnIndex) {
+		switch(columnIndex) {
+			case 0:
+				return getText();
+			case 1:
+				return getClazzName();
+			case 2:
+				return identifier;
+			default:
+				return null;
+		}
+
 	}
 	
 }
