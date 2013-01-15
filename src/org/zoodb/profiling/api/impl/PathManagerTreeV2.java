25f15458679bbf48b952e64ff485457f917566bc
tgmuender <tgmuender@gmail.com>
2013-01-15 18:38:30 +0100

    cleanup

--- src/org/zoodb/profiling/api/impl/PathManagerTreeV2.java
+++ src/org/zoodb/profiling/api/impl/PathManagerTreeV2.java
@@ -1,35 +1,22 @@
 package org.zoodb.profiling.api.impl;
 
-import java.util.Collection;
 import java.util.HashMap;
 import java.util.Iterator;
-import java.util.LinkedList;
-import java.util.List;
 import java.util.Map;
 
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.zoodb.profiling.api.AbstractActivation;
-import org.zoodb.profiling.api.Activation;
 import org.zoodb.profiling.api.IPathManager;
-import org.zoodb.profiling.api.tree.impl.AbstractNode;
-import org.zoodb.profiling.api.tree.impl.ClazzNode;
-import org.zoodb.profiling.api.tree.impl.NodeTraverser;
-import org.zoodb.profiling.api.tree.impl.ObjectNode;
-
 
 
 public class PathManagerTreeV2 implements IPathManager {
 	
-	private List<ObjectNode> objectLevelTrees;
-	private List<ClazzNode> classLevelTrees;
 	private Map<Class<?>,ActivationArchive> classArchives;
 	
 	private Logger logger = LogManager.getLogger("allLogger");
 	
 	public PathManagerTreeV2() {
-		objectLevelTrees = new LinkedList<ObjectNode>();
-		classLevelTrees = new LinkedList<ClazzNode>();
 		classArchives = new HashMap<Class<?>,ActivationArchive>();
 	}
 	
