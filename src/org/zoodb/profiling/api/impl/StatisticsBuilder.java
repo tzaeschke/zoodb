c17d91bd975bd863e5a63b5092e4d8ad20e93679
tgmuender <tgmuender@gmail.com>
2013-02-14 19:15:29 +0100

    export of access groups

--- src/org/zoodb/profiling/api/impl/StatisticsBuilder.java
+++ src/org/zoodb/profiling/api/impl/StatisticsBuilder.java
@@ -3,12 +3,16 @@ package org.zoodb.profiling.api.impl;
 import java.util.Collection;
 import java.util.Iterator;
 import java.util.LinkedList;
+import java.util.Map;
 
+import org.zoodb.jdo.internal.ZooClassDef;
 import org.zoodb.profiling.api.AbstractActivation;
 import org.zoodb.profiling.api.IFieldAccess;
 import org.zoodb.profiling.api.IFieldManager;
 import org.zoodb.profiling.api.IPathManager;
+import org.zoodb.profiling.api.Utils;
 
+import ch.ethz.globis.profiling.commons.statistics.AccessGroup;
 import ch.ethz.globis.profiling.commons.statistics.ClassStatistics;
 import ch.ethz.globis.profiling.commons.statistics.FieldStatistics;
 import ch.ethz.globis.profiling.commons.statistics.PathStatItem;
@@ -43,28 +47,68 @@ public class StatisticsBuilder {
 			long avgSize = Math.round(ProfilingManager.getInstance().getClassSizeManager().getClassStats(currentClass).getAvgClassSize());
 			currentCS.setAvgSize(avgSize);
 			
+			Collection<AccessGroup> accessGroups = new LinkedList<AccessGroup>();
 			
 			Iterator<AbstractActivation> ai = aa.getIterator();
 			
 			while (ai.hasNext()) {
 				AbstractActivation current = ai.next();
 				addToAccessors(current,accessors);
+				addToAccessGroups(current.getFas().values(), aa.getZooClassDef(), accessGroups);
 			}
+			//attach accessGroup
+			currentCS.setAccessGroups(accessGroups);
+			
 			//attache accessors to current class stats
 			for (PathStatItemActivation item : accessors) {
 				currentCS.addAccessor(item.toAPI());
 			}
-			
-			
-			
 			classStats.add(currentCS);
-			
 		}
 		
 		return classStats;
-		
 	}
 	
+	
+	private void addToAccessGroups(Collection<SimpleFieldAccess> fas,ZooClassDef ccdef, Collection<AccessGroup> accessGroups) {
+		//map idx to name
+		Collection<String> fNames = new LinkedList<String>();
+		for (SimpleFieldAccess sfa : fas) {
+			fNames.add(Utils.getFieldNameForIndex(sfa.getIdx(), ccdef));
+		}
+		
+		//check if this accessGroup already exists
+		
+		for (AccessGroup ag : accessGroups) {
+			if (ag.matchesGroup(fNames)) {
+				//update this group
+				for (SimpleFieldAccess sfa : fas)  {
+					String name = Utils.getFieldNameForIndex(sfa.getIdx(), ccdef);
+					ag.update(name, sfa.getrCount(), sfa.getwCount());
+				}
+				return;
+			}
+		}
+		
+		/*
+		 * at this point we have not found a group, create a new one and init with 'fas'
+		 */
+		Collection<FieldStatistics> fasAsFS = new LinkedList<FieldStatistics>();
+		
+		for (SimpleFieldAccess sfa : fas)  {
+			FieldStatistics fs = new FieldStatistics();
+			fs.setFieldName(Utils.getFieldNameForIndex(sfa.getIdx(), ccdef));
+			fs.setTotalReads(sfa.getrCount());
+			fs.setTotalWrites(sfa.getwCount());
+			
+			fasAsFS.add(fs);
+		}
+		
+		AccessGroup ag = new AccessGroup(fasAsFS);
+		accessGroups.add(ag);
+		
+	}
+
 	private void addToAccessors(AbstractActivation a, Collection<PathStatItemActivation> accessors) {
 		//check if there already exists an accessor with same (parentClazz,parentFieldName)
 		
