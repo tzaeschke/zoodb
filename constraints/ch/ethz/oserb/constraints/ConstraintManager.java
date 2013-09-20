b171d0b3d265ff2759d78709a2d1075f26ab3a1f
oserb <oserb@ARRAN.d.ethz.ch>
2013-09-20 12:08:37 +0200

    remove empty line (eclipse commit and push test)

--- constraints/ch/ethz/oserb/constraints/ConstraintManager.java
+++ constraints/ch/ethz/oserb/constraints/ConstraintManager.java
@@ -35,7 +35,6 @@ public class ConstraintManager {
 		/*OCL ocl = OCL.newInstance(EcoreEnvironmentFactory.INSTANCE);
 		OCLHelper<EClassifier, ?, ?, Constraint> oclHelper = ocl.createOCLHelper();*/
 		
-		
 		this.pm = pm;
 	}
 	public boolean checkConstraints(){
