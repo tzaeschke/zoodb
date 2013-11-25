d357a0b5bca081b4ed58e5c12ef491f7c044890f
oserb <oserb@student.ethz.ch>
2013-11-25 15:04:59 +0100

    immediate test
    test case for immediate evaluation.

--- constraints/ch/ethz/oserb/test/AllTests.java
+++ constraints/ch/ethz/oserb/test/AllTests.java
@@ -7,7 +7,7 @@ import org.junit.runners.Suite.SuiteClasses;
 @RunWith(Suite.class)
 @SuiteClasses({ test_foreign.class, test_ocl_annotation.class,
 		test_ocl_configurer.class, test_primary.class, test_profiles.class,
-		test_severity.class, test_unique.class, test_factory.class, test_pojo.class })
+		test_severity.class, test_unique.class, test_factory.class, test_pojo.class, test_immediate.class })
 public class AllTests {
 
 }
