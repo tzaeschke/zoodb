3569ccaad6464b8752b17ef3cfe727e31313905e
Tilmann <zoodb@gmx.de>
2016-02-24 22:37:37 +0100

    Updated examples

--- examples/org/zoodb/jdo/ex3/Example3Main.java
+++ examples/org/zoodb/jdo/ex3/Example3Main.java
@@ -18,7 +18,7 @@
  * 
  * See the README and COPYING files for further information. 
  */
-package org.zoodb.jdo.ex2;
+package org.zoodb.jdo.ex3;
 
 import java.util.Collection;
 
@@ -27,21 +27,32 @@ import javax.jdo.PersistenceManager;
 import javax.jdo.Query;
 
 import org.zoodb.jdo.ZooJdoHelper;
+import org.zoodb.jdo.ex2.Course;
+import org.zoodb.jdo.ex2.Person;
+import org.zoodb.jdo.ex2.School;
+import org.zoodb.jdo.ex2.Student;
+import org.zoodb.jdo.ex2.Teacher;
 
 /**
- * Slightly advanced example that uses inheritance, indexing and navigation.
- * The queries show path queries and the use of Java methods in queries.
- * Note that JDOQL / ZooDB support only some methods that are available in 
- * normal Java classes.
+ * This example that shows how to access object outside of transactions with
+ * non-transactional read. This is, for example, useful in GUI
+ * applications where a transaction should be closed most of the time,
+ * while the GUI should still be able to read data from the database.
+ * 
+ * Note that in a setting with multiple concurrent persistence managers,
+ * this may result in inconsistencies because
+ * any values that were read outside of a transaction may be outdated by
+ * the time when the next transaction commits. 
+ * 
  */
-public class Example2Main {
+public class Example3Main {
 
-	private static final String DB_FILE = "example2.zdb";
+	private static final String DB_FILE = "example3.zdb";
 
 	private PersistenceManager pm;
 	
 	public static void main(final String[] args) {
-		new Example2Main().run();
+		new Example3Main().run();
 	}
 
 	private void run() {
@@ -100,24 +111,17 @@ public class Example2Main {
 
 	private void executeQueries() {
 		pm = ZooJdoHelper.openDB(DB_FILE);
-		pm.currentTransaction().begin();
-
-		// All people (show names).
+		
+		//set non-transactional read to 'true', but don't begin() yet.
+		pm.currentTransaction().setNontransactionalRead(true);
+		
+		//use queries and navigation outside of a transaction:
 		queryForPeople();
-
-		// All courses (titles) with given teacher.
 		queryForCoursesByTeacher("Albert Einstein");
 
-		// The course with the most students.
-		queryForCoursesWithMaxStudentCount();
-
-		
-		// All courses with 2 students.
-		queryForCoursesWithXStudents(2);
-		
-		// Find all courses whose teacher have a first name and a last name
-		queryForCoursesWithTeachersWithFirstAndLastName();
-		
+		// now, if we want to change something, we can open a trancaction
+		pm.currentTransaction().begin();
+		// change something here
 		pm.currentTransaction().commit();
 		pm.close();
 		pm = null;
@@ -144,59 +148,6 @@ public class Example2Main {
 		for (Course c : courses) {
 			System.out.println(">> - " + c.getName() + " by " + c.getTeacher().getName());
 		}
-	}
-	
-	/**
-	 * This example demonstrates that using 'navigation'may often be a good alternative
-	 * for queries, especially if the term in question (collection.size()) cannot be indexed yet.
-	 */
-	private void queryForCoursesWithMaxStudentCount() {
-		//TODO `MAX` in queries doesn't work yet, so we use navigation
-		Extent<Course> courses = pm.getExtent(Course.class);
-		Course maxCourse = null;
-		int maxStudents = -1;
-		for (Course c: courses) {
-			if (c.getStudents().size() > maxStudents) {
-				maxStudents = c.getStudents().size();
-				maxCourse = c;
-			}
-		}
-		if (maxCourse != null) {
-			System.out.println(">> Query for course with most students returned: " +
-					maxCourse.getName());
-		} else {
-			System.out.println(">> Query for course with most students returned no courses.");
-		}
-	}
-	
-	/**
-	 * This example demonstrates how many Java methods of Java SE classes can
-	 * be used in queries. Not that not all methods can be used.
-	 */
-	@SuppressWarnings("unchecked")
-	private void queryForCoursesWithXStudents(int studentCount) {
-		System.out.println(">> Query for courses with " + studentCount + " students:");
-		//using Java method in query
-		Query q = pm.newQuery(Course.class, "students.size() == " + studentCount + "");
-		Collection<Course> courses = (Collection<Course>)q.execute(); 
-		for (Course c : courses) {
-			System.out.println(">> - " + c.getName() + " has size: " + c.getStudents().size());
-		}
-	}
+	}	
 
-	
-	/**
-	 * This example combines a path query with a Java method call on the String class.
-	 */
-	@SuppressWarnings("unchecked")
-	private void queryForCoursesWithTeachersWithFirstAndLastName() {
-		System.out.println(">> Query for courses whose teacher have a frist and last name:");
-		//using Java method in query
-		Query q = pm.newQuery(Course.class, "teacher.name.indexOf(' ') >= 1");
-		Collection<Course> courses = (Collection<Course>)q.execute(); 
-		for (Course c : courses) {
-			System.out.println(">> - " + c.getName() + 
-					" has teacher: " + c.getTeacher().getName());
-		}
-	}
 }
